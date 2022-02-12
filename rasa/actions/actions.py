8a3f1bc4b7a6ef72d0cdaa22824e293ce8636e01
Kelvin Yap <kelvin_yap0306@yahoo.com>
2022-02-13 05:18:08 +0800

    completed. remove all unused contents.

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -4,30 +4,6 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-
-
-
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -64,7 +40,6 @@ class ActionSubmit(Action):
         dispatcher.utter_message(
             template="utter_submit_details",
             Name=tracker.get_slot("my_name"),
-            # Mobile_number=tracker.get_slot("number"),
             Service=tracker.get_slot("my_service"),
             Breed=tracker.get_slot("my_breed")
         )
